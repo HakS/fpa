1eb17d97e962582c10c3c81fef6bb305afa74916
Corey Aufang <corey.aufang@gmail.com>
2010-03-26 23:22:45 +0000

    small naming updates. changed popup to present rows with correct coloring

--- fpa_parent.js
+++ fpa_parent.js
@@ -8,7 +8,12 @@ jQuery(function ($) {
       height: 600,
       autoFit: false,
       onLoad: function (self, iFrameWindow, iFrameDocument) {
-        $("td.permission:contains('" + Drupal.settings.fpa_field + "')", iFrameDocument).parents('tr').addClass('fpa_show');
+        var odd_or_even = 'even';
+        $("td.permission:contains('" + Drupal.settings.fpa_perm + "')", iFrameDocument).parents('tr').addClass('fpa_show');
+        $(".fpa_show", iFrameDocument).each(function(index) {
+          odd_or_even = odd_or_even == 'even' ? 'odd' : 'even';
+          $(this).removeClass('odd even').addClass(odd_or_even);
+        });
       }
     });
     return false;
