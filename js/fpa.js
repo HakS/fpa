997a5dfd8d32310fafb8819020b8cbd5e4c04dac
Corey Aufang <corey.aufang@gmail.com>
2011-03-14 17:10:27 -0700

    by corey.aufang: preparation for permission search

--- js/fpa.js
+++ js/fpa.js
@@ -1,4 +1,16 @@
 (function ($) {
+  Drupal.behaviors.fpa_prepare = function (context) {
+    var module_id, new_module_id;
+    $("#permissions tbody tr", context).each(function () {
+      new_module_id = $(this).find("td.module").attr("id");
+      if (new_module_id) {
+        module_id = new_module_id;
+      }
+      else {
+        $(this).data('fpa_module', module_id);
+      }
+    });
+  };
   Drupal.behaviors.fpa = function (context) {
     var rows = $("#permissions", context)
     .find("td.permission")
@@ -8,9 +20,11 @@
     .removeClass('odd even')
     .filter(":even").addClass('odd').end()
     .filter(":odd").addClass('even').end()
-    .addClass('fpa_show').each(function () {
-      $(this).prevAll("tr:has(td.module):first").addClass('fpa_show');
-    });
+    .addClass('fpa_show')
+    .each(function () {
+      $('#'+$(this).data('fpa_module'), context).parent().addClass('fpa_show');
+    })
+    ;
   };
   Drupal.behaviors.fpa_modalframe = function (context) {
     $('a.fpa_modalframe', context).click(function (e) {
@@ -23,3 +37,4 @@
     });
   };
 })(jQuery);
+
