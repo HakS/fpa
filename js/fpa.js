b2113309d58ce71bee1fb6eee3969dab3ecd50cf
corey.aufang <corey.aufang@gmail.com>
2011-03-27 01:40:57 -0700

    by corey.aufang: refactoring to be usable by other modules.

--- js/fpa.js
+++ js/fpa.js
@@ -1,90 +1,90 @@
 (function ($) {
-  var fpa_table,
-      fpa_rows,
-      fpa_perms;
+  var fpa = {};
   
-  Drupal.behaviors.fpa_prepare = {
-    attach: function (context) {
-      var module_id, new_module_id, timeout;
-      
-      fpa_table = $("#permissions", context);
-      fpa_rows = fpa_table.find('tbody tr');
-      fpa_perms = fpa_rows.find('td.permission');
-      
-      fpa_rows
+  fpa.prepare = function (context) {
+    var module_id, new_module_id;
+    
+    fpa.table = $("#permissions", context);
+    fpa.rows = fpa.table.find('tbody tr');
+    fpa.perms = fpa.rows.find('td.permission');
+    
+    fpa.rows
+      .each(function () {
+        new_module_id = $(this).find("td.module").attr("id");
+        if (new_module_id) {
+          module_id = new_module_id;
+        }
+        else {
+          $(this).data('fpa_module', module_id);
+        }
+      });
+    $('<input id="fpa_search" type="text" class="form-text" />')
+      .insertBefore(fpa.table.parent())
+      .keypress(function (e) {
+        //prevent enter from submitting form
+        if (e.which == 13) {
+          return false;
+        }
+      })
+      .keyup(function (e) {
+        var $val = $(this).val();
+        if ($val != '') {
+          fpa.rows.css('display', 'none');
+          Drupal.settings.fpa.perm = $val;
+          fpa.filter(window.document);
+        }
+        else {
+          fpa.rows
+            .css('display', '')
+            .removeClass('odd even')
+            .filter(":even").addClass('odd').end()
+            .filter(":odd").addClass('even').end();
+        }
+      })
+      .wrap('<div class="form-item" class="form-type-textfield" />')
+      .before('<label for="fpa_search">Search:</label>')
+      .after('<div class="description">Start typing and only permissions that contain the entered text will be displayed.</div>')
+      .val(Drupal.settings.fpa.perm);
+  };
+  
+  fpa.filter = function () {
+    var perm_labels = {};
+    if (typeof Drupal.settings.fpa.perm != 'undefined' && Drupal.settings.fpa.perm.length > 0) {
+      fpa.rows.css('display', 'none');
+      fpa.perms
+        .filter(function () {
+          return $(this).text().toLowerCase().indexOf(Drupal.settings.fpa.perm.toLowerCase()) != -1;
+        }).parent()
+        .css('display', '')
         .each(function () {
-          new_module_id = $(this).find("td.module").attr("id");
-          if (new_module_id) {
-            module_id = new_module_id;
-          }
-          else {
-            $(this).data('fpa_module', module_id);
-          }
+          perm_labels[$(this).data('fpa_module')] = 1;
         });
-      $('<input id="fpa_search" type="text" class="form-text" />')
-        .insertBefore(fpa_table.parent())
-        .keypress(function (e) {
-          //prevent enter from submitting form
-          if (e.which == 13) {
-            return false;
-          }
-        })
-        .keyup(function (e) {
-          var $val = $(this).val();
-          if ($val != '') {
-            fpa_rows.css('display', 'none');
-            Drupal.settings.fpa_perm = $val;
-            Drupal.behaviors.fpa.attach(window.document);
-          }
-          else {
-            fpa_rows
-              .css('display', '')
-              .removeClass('odd even')
-              .filter(":even").addClass('odd').end()
-              .filter(":odd").addClass('even').end();
-          }
-        })
-        .wrap('<div class="form-item form-type-textfield" />')
-        .before('<label for="fpa_search">Search:</label>')
-        .after('<div class="description">Start typing and only permissions that contain the entered text will be displayed.</div>')
-        .val(Drupal.settings.fpa_perm);
+      for (var i in perm_labels) {
+        $('#' + i).parent().css('display', '');
+      }
+      fpa.rows
+        .filter(':visible').removeClass('odd even')
+        .filter(":even").addClass('odd').end()
+        .filter(":odd").addClass('even').end();
     }
   };
+  
+  fpa.modalframe = function (context) {
+    $('a.fpa_modalframe', context).click(function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      Drupal.modalFrame.open({
+        url: $(this).attr('href'),
+        draggable: false
+      });
+    });
+  };
+  
   Drupal.behaviors.fpa = {
     attach: function (context) {
-      var perm_labels = {};
-      if (typeof Drupal.settings.fpa_perm != 'undefined' && Drupal.settings.fpa_perm.length > 0) {
-        fpa_rows.css('display', 'none');
-        var ti = new Date().getTime();
-        fpa_perms
-          .filter(function () {
-            return $(this).text().toLowerCase().indexOf(Drupal.settings.fpa_perm.toLowerCase()) != -1;
-          }).parent()
-          .css('display', '')
-          .each(function () {
-            perm_labels[$(this).data('fpa_module')] = 1;
-          });
-        console.log(new Date().getTime() - ti);
-        for (var i in perm_labels) {
-          $('#' + i).parent().css('display', '');
-        }
-        fpa_rows
-          .filter(':visible').removeClass('odd even')
-          .filter(":even").addClass('odd').end()
-          .filter(":odd").addClass('even').end();
-      }
-    }
-  };
-  Drupal.behaviors.fpa_modalframe = {
-    attach: function (context) {
-      $('a.fpa_modalframe', context).click(function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        Drupal.modalFrame.open({
-          url: $(this).attr('href'),
-          draggable: false
-        });
-      });
+      fpa.prepare(context);
+      fpa.filter();
+      fpa.modalframe(context);
     }
   };
 })(jQuery);
